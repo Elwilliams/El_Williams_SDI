00a0254ab55bbdc4cf42eb206cc1ee6326bdf1c7
Erica Williams <elwilliams2@fullsail.edu>
2015-07-09 21:57:25 -0400

    Week 1 Output_assignment
    First Java and html!

---
+++ Webstorm files/WebstormProjects/Javascript try 2/Willams_EL_output_assign.js
@@ -0,0 +1,22 @@
+/**
+ * Created by EWilliams on 7/9/15.
+ */
+{
+var yearCreated; //declaration
+      yearCreated = 2015; //definition
+      console.log (yearCreated);
+
+var completed; //declaration
+      completed = ("appearsInConsole"); //definition
+      console.log (completed);
+
+var itisnot2015 = false; //boolean variable
+
+var itIs2015 = true; //boolean variable
+
+    alert ("IfThisShowsIveGotIt");
+
+//storage container = value
+
+
+}
