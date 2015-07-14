ba4806dfae6746967572ae71220e4dd234f37ed9
Erica Williams <elwilliams2@fullsail.edu>
2015-07-14 00:50:40 -0400

    Wk 2 Practice
    math applications in java

---
+++ Webstorm files/WebstormProjects/Week 2 Pracatice/js/E_Williams_Wk2_Practice.js
@@ -0,0 +1,16 @@
+/**
+ * Created by EWilliams on 7/13/15.
+ */
+var a =65; //declaring a variable
+var b = a + 12; //addition
+console.log(b);
+
+var c = a - 22;//subtraction
+console.log (c);
+
+var d = a*2;//multiplication
+
+var e = a/2;//division
+console.log (d);
+console.log (e);
+
