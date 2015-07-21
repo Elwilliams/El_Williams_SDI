950f206cddcfd11a5761c16c2c07264cb2796e2c
Erica Williams <elwilliams2@fullsail.edu>
2015-07-21 04:11:50 -0400

    Final Draft
    Here is the final draft recommitted again

---
+++ Williams_El_Expressions_java.js
@@ -0,0 +1,86 @@
+/**
+ * Created by EWilliams on 7/14/15.
+
+//Erica Williams Expressions Assignment
+//SDI week 2 assignment 7/13/2015
+ // Area of a Trapezoid is (b1+b2)/2 times h
+*/
+//Sample hard value trapezoid formula
+var b1 = 10; //definition
+var b2 = 12; //definition
+console.log (b1);//print to console
+console.log (b2);//print to console
+
+var basearray = [b1, b2];//array
+console.log (basearray);//print to console
+
+var total= b1 + b2; //addition step 1 in formula
+console.log (total); //print to console
+
+var h = 14;//definition
+console.log (h);//print to console
+
+area = (total/2)*h;//definition of formula
+console.log (area); //print to console
+
+//This is a working formula for the area of a trapezoid for edge 1 10, edge 2 12, height 10
+
+//Prompts for a user to find the area of a trapezoid
+
+alert("To find the area of a trapezoid \nPlease Enter Base Edge 1, Base Edge 2, and Height in the next three windows"); //alert of intention
+console.log ("Please Enter Base Edge 1, Base Edge 2, and the Height of the Trapezoid in the next 3 windows");//print to console
+
+var userb1 = prompt ("Please Enter Base Edge 1"); //prompt 1 for the user to get an area
+console.log ("Please Enter Base Edge 1");//print to console
+console.log (userb1);//print to console
+
+var userb2 = prompt ("Please Enter Base Edge 2"); // prompt 2 for the user to get an area
+console.log ("Please Enter Base Edge 2");//print to console
+console.log (userb2); //print to console
+
+var userh = prompt ("Please Enter Height"); // prompt 3 for the user to get an area
+console.log ("Please Enter Height");//print to console
+console.log (userh);//print to console
+
+alert ("The is area of the Trapazoid is");//alerting user the next field has the answer
+console.log ("The is area of the Trapazoid is");//printing to console
+
+var usertotal = Number(userb1)+ Number(userb2);//step 1 in formula for prompts
+console.log (usertotal);//print to console
+
+var userarea = (usertotal)/2 * userh; //solution prompt for user
+prompt (userarea);//show user the area of the trapezoid
+console.log (userarea);//print to console
+
+//Assignment Operators requirement added in this section
+var noofonions = 3;//definition
+console.log (noofonions); //print to console
+noofonions += 4;//addition assignment operator
+console.log (noofonions);//print to console
+
+var noofpears = 5;// definition
+noofpears -= 5;//subtraction assignment operator
+console.log (noofpears); //print to console
+
+//Assignmnet Operators end //
+
+//End of assignment//
+//Recommitting again//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
