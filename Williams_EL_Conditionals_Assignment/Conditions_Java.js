055501f2c78e7b944834f364eb0816f052910fb9
Erica Williams <elwilliams2@fullsail.edu>
2015-07-22 01:41:13 -0400

    Commit 1
    Starting code for week 3 conditionals java and html. at this point i
    have a working java link and the beginning of an idea for if else and
    else if

---
+++ Williams_EL_Conditionals_Assignment/Conditions_Java.js
@@ -0,0 +1,18 @@
+/**
+ * Created by EWilliams on 7/21/15.
+ */
+// Erica WIlliams 7/21/2015//
+//SDI Section 2//
+//Conditional Assignmentt Week 3//
+
+//The formula for the area of a square is side * side//
+//The formula for the area of a rectangle is length * width//
+//The formula for the area of a cube is 6 * (side*side*side)//
+
+alert ("To find the area of one of 3 shapes, please enter Square, Rectangle, or Cube");// begin user input
+console.log ("To find the area of one of 3 shapes, please enter Square, Rectangle, or Cube"); //print to console
+
+Prompt ("Choose your shape");//Select formula to use
+console.log ("Choose your shape");//print to console
+rectangle = length * width; //establish formula
+square =2;
