75e319aec27b6983cd43e1d4b36f48bf0fba26bf
Erica Williams <elwilliams2@fullsail.edu>
2015-07-22 02:46:42 -0400

    Commit 3
    Trying to figure out to have a variable in user input that starts a if
    else.

--- Williams_EL_Conditionals_Assignment/Conditions_Java.js
+++ Williams_EL_Conditionals_Assignment/Conditions_Java.js
@@ -8,9 +8,37 @@
 //The formula for the area of a square is side * side//
 //The formula for the area of a rectangle is length * width//
 
-alert ("To find the perimeter of one of 2 shapes, please enter Square or Rectangle perimeter");// begin user input
-console.log ("To find the perimeter of one of 2 shapes, please enter Square or Rectangle perimeter"); //print to console
+alert ("To find the perimeter of one of 2 shapes, please enter Square or Rectangle");// begin user input
+console.log ("To find the perimeter of one of 2 shapes, please enter Square or Rectangle"); //print to console
 
-Prompt ("Choose your shape");//Select formula to use
-console.log ("Choose your shape");//print to console
+var userShape = prompt ("Enter either square or rectangle");
+console.log ("Enter either square or rectangle");
+console.log (userShape);
 
+// We have an alert a prompt to make a decision and print of that decision //
+
+var Square = shapeOne
+console.log (Square)
+
+var Rectangle = shapeTwo;
+console.log (Rectangle
+
+if (Square) {
+    userSHapeA = prompt ("Enter Shape");
+    console.log ("Enter Shape")
+
+    var side = Prompt ("Enter one side's length of the square");//Select formula to use
+    console.log ("Enter one side's length of the square");
+    console.log (side);
+
+}else{ (Rectangle);
+    shape = prompt ("Enter Shape");
+    var shape = rectangle;
+    shortSide = prompt ("Enter the short side of the rectangle");
+    console.log (shortside);
+    longSide = prompt ("Enter the long side of the rectangle");
+    console.log (longside);
+}
+
+    var form1 = 2 * Number(sideshort) * 2 * Number(sidelong);
+    var form2 = side * side * side * side;
