73385510838fa76a3aed029fe25af7e1d816dc99
Erica Williams <elwilliams2@fullsail.edu>
2015-07-22 01:57:00 -0400

    Commit 2
    Change of direction because I need 2 operations and areas on have
    multiplication.

--- Williams_EL_Conditionals_Assignment/Conditions_Java.js
+++ Williams_EL_Conditionals_Assignment/Conditions_Java.js
@@ -7,12 +7,10 @@
 
 //The formula for the area of a square is side * side//
 //The formula for the area of a rectangle is length * width//
-//The formula for the area of a cube is 6 * (side*side*side)//
 
-alert ("To find the area of one of 3 shapes, please enter Square, Rectangle, or Cube");// begin user input
-console.log ("To find the area of one of 3 shapes, please enter Square, Rectangle, or Cube"); //print to console
+alert ("To find the perimeter of one of 2 shapes, please enter Square or Rectangle perimeter");// begin user input
+console.log ("To find the perimeter of one of 2 shapes, please enter Square or Rectangle perimeter"); //print to console
 
 Prompt ("Choose your shape");//Select formula to use
 console.log ("Choose your shape");//print to console
-rectangle = length * width; //establish formula
-square =2;
+
