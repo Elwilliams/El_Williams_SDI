9eb510cbb988da8b167a936f419a869fe6fc8c50
Erica Williams <elwilliams2@fullsail.edu>
2015-07-23 16:24:07 -0400

    Tweaking the files
    Reassigned the files in correct format

---
+++ Williams_EL_Conditionals_Assignment/js/Condition_LastJava.js
@@ -0,0 +1,55 @@
+/**
+ * Created by EWilliams on 7/23/15.
+ */
+/**
+ * Created by EWilliams on 7/21/15.
+ */
+// Erica Williams 7/21/2015//
+//SDI Section 2//
+//Conditional Assignment Week 3//
+
+alert("Are you eligible for a discount? Let's Find out!"); //alert of intention //first user input
+console.log ("Are you eligible for a discount? Let's Find out!");//print to console
+
+var UserAmt = Prompt("Please enter your total purchase amount");//First User Prompt
+    console.log ("Enter your total purchase amount"); //print to console
+    console.log (UserAmt);
+
+if  (UserAmt === 200) {
+    console.log("You are not eligible for a discount.");
+}else if (UserAmount ===5000);
+        console.log ("You get a 100 dollar discount");
+}else if (UserAmount === 10000)) {
+        console.log ("You get a 500 dollar discount");
+}else if (UserAmount === 50001);
+        console.log("You get a 1000 dollar discount");
+    }
+
+alert ("If this purchase did not meet the requirement, We have another discount type!");
+console.log ("If this purchase did not meet the requirement, We have another discount type!");
+
+    var UserMul = (Prompt("How many purchases have you made this year? ")); // Second user prompt
+    if (UserMul = 0){
+        var One =Number(UserMal - 0);
+        console.log (One);
+        alert ("You do not qualify for a discount. /n Answer the next question to qualify for the last discount");
+        Console.log ("You do not qualify for a discount. /n Answer the next question to qualify for the last discount");}
+
+else {
+        (UserMul <1) ;
+        var Two = Number(Usermul*10);
+        Console.log (Two);
+        alert ("You qualify for a 10% Discount!");// end second prompt
+    }
+alert ("Are you a new account?");
+console.log ("Are you a new account");
+
+var New = Prompt ("Enter Yes or NO");
+console.log ("Enter Yes or No");
+
+if  (New = Yes){
+    Console.log ("You get a 10% Discount");
+}else{ (New = NO)};
+    Console.log ("I am sorry you do not qualify");
+}
+
--- Williams_EL_Conditionals_Assignment/js/Conditions_Java.js
+++ Williams_EL_Conditionals_Assignment/js/Conditions_Java.js
@@ -5,24 +5,30 @@
 //SDI Section 2//
 //Conditional Assignment Week 3//
 
-//The formula for the area of a square is side * side//
-//The formula for the area of a rectangle is length * width//
-
 alert ("Are you eligible for a discount based on your purchase amount");// begin user input
 console.log ("Are you eligible for a discount based on your purchase amount"); //print to console
 
 var UserAmount = (Number (Prompt("Enter your total purchase amount"));
-    console. log ("Enter your total purchase amount");
+console. log ("Enter your total purchase amount");
 
-if  (userAmount > 200){
+if  (UserAmount > 200){
     console.log("You are not eligible for a discount.");
-    } else if (UserAmount < 200 >5000) {
+} else if (UserAmount < 200 >5000) {
     console.log ("You get a 100 dollar discount");
-    } else if (UserAmount < 5000 > 50000) {
+} else if (UserAmount < 5000 > 50000) {
     console.log ("You get a 500 dollar discount");
-    } else if (UserAmount < 50001) {
+} else (UserAmount < 50001) {
     console.log("You get a 1000 dollar discount");
 
     alert ("If this purchase did not meet the requirement, We have another discount type!")
 
-var UserMultiple = (Number= (Prompt )
+    var UserMultiple = (Number(Prompt("How many purchases have you made this year? ")
+    if (1){
+        var One =Number(UserMultiple - 0);
+        console.log (One);
+        alert ("You do not qualify for a discount. /n After another purchase you get a $25.00 Discount");
+        Console.log ("You do not qualify for a discount. /n After another purchase you get a $25.00 Discount");}
+}
+
+//test
+
