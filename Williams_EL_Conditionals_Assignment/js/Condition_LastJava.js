6991db16e433de8d0ca98cdcc0848d6ac8f0dca6
Erica Williams <elwilliams2@fullsail.edu>
2015-07-23 19:17:14 -0400

    A commit before finall
    NO lines just an if else issue that needs to be resolved

--- Williams_EL_Conditionals_Assignment/js/Condition_LastJava.js
+++ Williams_EL_Conditionals_Assignment/js/Condition_LastJava.js
@@ -1,55 +1,41 @@
 /**
  * Created by EWilliams on 7/23/15.
  */
-/**
- * Created by EWilliams on 7/21/15.
- */
 // Erica Williams 7/21/2015//
 //SDI Section 2//
 //Conditional Assignment Week 3//
 
-alert("Are you eligible for a discount? Let's Find out!"); //alert of intention //first user input
-console.log ("Are you eligible for a discount? Let's Find out!");//print to console
+//formula to calculate hotel cost is noofnights * typeofroom + extras//
 
-var UserAmt = Prompt("Please enter your total purchase amount");//First User Prompt
-    console.log ("Enter your total purchase amount"); //print to console
-    console.log (UserAmt);
+alert("The cost to stay at the resort can be calculated by answering the following questions"); //alert of intention //first user input
+console.log ("The cost to stay at the resort can be calculated by answering the following questions");//print to console
 
-if  (UserAmt === 200) {
-    console.log("You are not eligible for a discount.");
-}else if (UserAmount ===5000);
-        console.log ("You get a 100 dollar discount");
-}else if (UserAmount === 10000)) {
-        console.log ("You get a 500 dollar discount");
-}else if (UserAmount === 50001);
-        console.log("You get a 1000 dollar discount");
-    }
+var noofnights = prompt ("How many nights would you like to stay?");//first user prompt
+console.log ("How many nights would you like to stay");  //print to console
+console.log (noofnights);
 
-alert ("If this purchase did not meet the requirement, We have another discount type!");
-console.log ("If this purchase did not meet the requirement, We have another discount type!");
+if (noofnights === "") { //if user enters nothing
+    noofnights = prompt("Please enter the number of nights to proceed");
+    console.log("Please enter the number of nights to proceed")
 
-    var UserMul = (Prompt("How many purchases have you made this year? ")); // Second user prompt
-    if (UserMul = 0){
-        var One =Number(UserMal - 0);
-        console.log (One);
-        alert ("You do not qualify for a discount. /n Answer the next question to qualify for the last discount");
-        Console.log ("You do not qualify for a discount. /n Answer the next question to qualify for the last discount");}
+}else{
+    noofnights = Number(""); //if user enters a value
+    console.log (noofnights); //print to console
 
-else {
-        (UserMul <1) ;
-        var Two = Number(Usermul*10);
-        Console.log (Two);
-        alert ("You qualify for a 10% Discount!");// end second prompt
-    }
-alert ("Are you a new account?");
-console.log ("Are you a new account");
-
-var New = Prompt ("Enter Yes or NO");
-console.log ("Enter Yes or No");
-
-if  (New = Yes){
-    Console.log ("You get a 10% Discount");
-}else{ (New = NO)};
-    Console.log ("I am sorry you do not qualify");
 }
 
+var typeofroom = prompt ("For Regular Room enter 50 or For Deluxe Room enter 100");
+console.log ("For Regular Room enter 50 or For Deluxe Room enter 100");
+console.log (typeofroom);//print to console
+
+var extras = prompt ("For Extras pack 1 enter 50 for Extras pack 2 enter 20");
+console.log ("For Extras pack 1 enter 50 for Extras pack 2 enter 20");
+console.log (extras);
+
+var nightsCost = Number(noofnights) * Number(typeofroom);//calculation 1 of formula
+console.log (nightsCost);// print to console
+
+var totalCost = Number(nightsCost) + Number(extras);//formula calculation for total cost
+console.log (totalCost);//print to console
+
+prompt (totalCost); //show user the total cost
