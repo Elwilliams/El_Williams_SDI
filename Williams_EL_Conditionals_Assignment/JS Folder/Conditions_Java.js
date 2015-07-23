cac22c51d4333a01481441bde327c0f9e5366050
Erica Williams <elwilliams2@fullsail.edu>
2015-07-22 23:53:22 -0400

    js/html file
    made file structure but now the files are gone from my web storm.

--- Williams_EL_Conditionals_Assignment/JS Folder/Conditions_Java.js
+++ Williams_EL_Conditionals_Assignment/JS Folder/Conditions_Java.js
@@ -8,23 +8,20 @@
 //The formula for the area of a square is side * side//
 //The formula for the area of a rectangle is length * width//
 
-alert ("To find the perimeter of one of 2 shapes, please enter Square or Rectangle");// begin user input
+alert ("To find the perimeter of one of 2 shapes, Please enter Square or Rectangle");// begin user input
 console.log ("To find the perimeter of one of 2 shapes, please enter Square or Rectangle"); //print to console
 
 var userShape = prompt ("Enter either square or rectangle");
 console.log ("Enter either square or rectangle");
 console.log (userShape);
 
-// We have an alert a prompt to make a decision and print of that decision //
+// We have an alert, a prompt to make a decision, and a print of that decision //
 
-var Square = shapeOne
-console.log (Square)
-
-var Rectangle = shapeTwo;
-console.log (Rectangle
-
-if (Square) {
-    alert (Square);
-}else{ (Rectangle);
-    alert Rectangle;
+if  ("square");{
+    var square = 1;
+    console.log (square);
+}(else{
+    ("Rectangle")
+    var rectangle = 2;
+    console.log (rectangle);
 }
