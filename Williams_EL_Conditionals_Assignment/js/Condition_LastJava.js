2dfb08464015099dbdab8e711465039dda20edb2
Erica Williams <elwilliams2@fullsail.edu>
2015-07-23 21:33:19 -0400

    Almost there
    Got the ternary in there and fixed the if else issue

--- Williams_EL_Conditionals_Assignment/js/Condition_LastJava.js
+++ Williams_EL_Conditionals_Assignment/js/Condition_LastJava.js
@@ -8,31 +8,36 @@
 //formula to calculate hotel cost is noofnights * typeofroom + extras//
 
 alert("The cost to stay at the resort can be calculated by answering the following questions"); //alert of intention //first user input
-console.log ("The cost to stay at the resort can be calculated by answering the following questions");//print to console
+console.log ("The cost to stay at the resort can be calculated by answering the following questions");//print to console the alert statement
 
-var noofnights = prompt ("How many nights would you like to stay?");//first user prompt
-console.log ("How many nights would you like to stay");  //print to console
-console.log (noofnights);
+var noOfNights = prompt ("How many nights would you like to stay?");//first user prompt
+console.log ("How many nights would you like to stay");  //print to console the statement
+console.log (noOfNights);//print to console the number value
 
-if (noofnights === "") { //if user enters nothing
-    noofnights = prompt("Please enter the number of nights to proceed");
-    console.log("Please enter the number of nights to proceed")
+(noOfNights === "")? console.log("Whoops, Please refresh the page and start again"):console.log("Great, Lets move on to the next question"); //Ternary for not entering a value in NoOfNights//
+alert("Whoops, Please refresh the page and start again, Your cost will not be correct if you continue");//user error message to restart
 
-}else{
-    noofnights = Number(""); //if user enters a value
-    console.log (noofnights); //print to console
+var typeOfRoom = prompt ("For Regular Room enter 50 or For Deluxe Room enter 100");// User prompt 2
+console.log ("For Regular Room enter 50 or For Deluxe Room enter 100"); //print to console
+console.log (typeOfRoom);//print to console
 
+if  (typeOfRoom === ""){
+    typeOfRoom = prompt ("Whoops, please enter either 50 or 100");
+    console.log ("Whoops, please enter either 50 or 100")
+}else if
+    (typeOfRoom < "50"){
+    typeOfRoom = prompt ("Only a value of 50 or 100 can be entered");
+    console.log ("Only a value of 50 or 100 can be entered")
+}else if
+    (typeOfRoom === "1000"){
+    typeOfRoom = prompt ("Only a value of 50 or 100 can be entered");
+    console.log ("Only a value of 50 or 100 can be entered")
 }
+var extras = prompt ("For Extras pack 1 enter 50 for Extras pack 2 enter 20");//user prompt 3
+console.log ("For Extras pack 1 enter 50 for Extras pack 2 enter 20");// print to console statement
+console.log (extras);// print to console value for extras
 
-var typeofroom = prompt ("For Regular Room enter 50 or For Deluxe Room enter 100");
-console.log ("For Regular Room enter 50 or For Deluxe Room enter 100");
-console.log (typeofroom);//print to console
-
-var extras = prompt ("For Extras pack 1 enter 50 for Extras pack 2 enter 20");
-console.log ("For Extras pack 1 enter 50 for Extras pack 2 enter 20");
-console.log (extras);
-
-var nightsCost = Number(noofnights) * Number(typeofroom);//calculation 1 of formula
+var nightsCost = Number(noOfNights) * Number(typeOfRoom);//calculation 1 of formula
 console.log (nightsCost);// print to console
 
 var totalCost = Number(nightsCost) + Number(extras);//formula calculation for total cost
