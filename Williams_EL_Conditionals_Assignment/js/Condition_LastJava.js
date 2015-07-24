5585d57e408f25a88bd8ec5d77976fb673a60696
Erica Williams <elwilliams2@fullsail.edu>
2015-07-23 22:38:06 -0400

    Commit for working on logical operator
    Everything worked before the logical operator

--- Williams_EL_Conditionals_Assignment/js/Condition_LastJava.js
+++ Williams_EL_Conditionals_Assignment/js/Condition_LastJava.js
@@ -15,24 +15,20 @@ console.log ("How many nights would you like to stay");  //print to console the 
 console.log (noOfNights);//print to console the number value
 
 (noOfNights === "")? console.log("Whoops, Please refresh the page and start again"):console.log("Great, Lets move on to the next question"); //Ternary for not entering a value in NoOfNights//
-alert("Whoops, Please refresh the page and start again, Your cost will not be correct if you continue");//user error message to restart
 
 var typeOfRoom = prompt ("For Regular Room enter 50 or For Deluxe Room enter 100");// User prompt 2
 console.log ("For Regular Room enter 50 or For Deluxe Room enter 100"); //print to console
 console.log (typeOfRoom);//print to console
 
-if  (typeOfRoom === ""){
-    typeOfRoom = prompt ("Whoops, please enter either 50 or 100");
-    console.log ("Whoops, please enter either 50 or 100")
-}else if
-    (typeOfRoom < "50"){
-    typeOfRoom = prompt ("Only a value of 50 or 100 can be entered");
-    console.log ("Only a value of 50 or 100 can be entered")
-}else if
-    (typeOfRoom === "1000"){
-    typeOfRoom = prompt ("Only a value of 50 or 100 can be entered");
-    console.log ("Only a value of 50 or 100 can be entered")
+if  (typeOfRoom === "") {
+    typeOfRoom = prompt("Whoops, please enter either 50 or 100");
+    console.log("Whoops, please enter either 50 or 100");
+}else
+    (typeOfRoom =="50" && ="100" ){
+    typeOfRoom = prompt("Great, Let's keep going");
+    console.log ("Great, Lets Keep going");
 }
+
 var extras = prompt ("For Extras pack 1 enter 50 for Extras pack 2 enter 20");//user prompt 3
 console.log ("For Extras pack 1 enter 50 for Extras pack 2 enter 20");// print to console statement
 console.log (extras);// print to console value for extras
